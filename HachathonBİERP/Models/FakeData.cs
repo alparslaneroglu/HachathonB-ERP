b23c6d0d271efbd4360b1a9b6cb4b0118a32a28a
alparslaneroglu <furkanalparslaneroglu@gmail.com>
2022-03-21 14:49:52 +0300

    Add project files.

---
+++ HachathonBİERP/Models/FakeData.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HachathonBİERP.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Entity
+    {
+        public class FakeData
+        {
+            public List<MevcutKayitliVeri> KayitliVeri
+            {
+                get
+                {
+                    return new List<MevcutKayitliVeri> //Ürünler Ekledik
+                {
+                    new MevcutKayitliVeri
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        KayitNo = 1,
+                        BaslangicTarih = new DateTime(2020,05,23,7,30,0),
+                        BitisTarih = new DateTime(2020,05,23,8,30,0),
+                        ToplamSure = 1,
+                        Statu = "URETIM",
+
+                    },
+                    new MevcutKayitliVeri
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        KayitNo = 2,
+                        BaslangicTarih = new DateTime(2020,05,23,8,30,0),
+                        BitisTarih = new DateTime(2020,05,23,12,00,0),
+                        ToplamSure = 3.30M,
+                        Statu = "URETIM",
+
+                    },
+                    new MevcutKayitliVeri
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        KayitNo = 3,
+                        BaslangicTarih = new DateTime(2020,05,23,12,00,0),
+                        BitisTarih = new DateTime(2020,05,23,13,00,0),
+                        ToplamSure = 1,
+                        Statu = "URETIM",
+
+                    },
+                    new MevcutKayitliVeri
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        KayitNo = 4,
+                        BaslangicTarih = new DateTime(2020,05,23,13,00,0),
+                        BitisTarih = new DateTime(2020,05,23,13,45,0),
+                        ToplamSure = 0.45M,
+                        Statu = "DURUŞ",
+                        DurusNeden = "ARIZA"
+
+                    },
+                    new MevcutKayitliVeri
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        KayitNo = 5,
+                        BaslangicTarih = new DateTime(2020,05,23,13,45,0),
+                        BitisTarih = new DateTime(2020,05,23,17,30,0),
+                        ToplamSure = 3.45M,
+                        Statu = "URETIM",
+
+                    },
+
+                };
+
+                }
+
+            }
+            public List<StandartSituation> StandartSituation
+            {
+                get
+                {
+                    return new List<StandartSituation>
+                {
+                    new StandartSituation()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        StartDate = new DateTime(2020,05,23,10,0,0),
+                        FinishDate = new DateTime(2020,05,23,10,15,0),
+                        Status = "ÇAY MOLASI"
+                    },
+                    new StandartSituation()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        StartDate = new DateTime(2020,05,23,12,0,0),
+                        FinishDate = new DateTime(2020,05,23,12,30,0),
+                        Status = "YEMEK MOLASI"
+                    },
+                    new StandartSituation()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        StartDate = new DateTime(2020,05,23,15,0,0),
+                        FinishDate = new DateTime(2020,05,23,15,15,0),
+                        Status = "ÇAY MOLASI"
+                    },
+                };
+                }
+            }
+
+
+
+
+        }
+    }
+
+}
---
+++ HachathonBİERP/Models/StandartDuruslar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HachathonBİERP.Models
+{
+    public class StandartDuruslar
+    {
+        public int Id { get; set; }
+        public DateTime Baslangıc { get; set; }
+        public DateTime Bitis { get; set; }
+        public string DurusNedeni { get; set; }
+
+    }
+    public class StandartDuruslarTablo
+    {
+        public List<StandartDuruslar> tbl = new List<StandartDuruslar>()
+        {
+            new StandartDuruslar(){Id=1,Baslangıc=new DateTime(0,0,0,10,0,0),Bitis=new DateTime(10,15,0),DurusNedeni="Çay Molası"},
+            new StandartDuruslar(){Id=2,Baslangıc=new DateTime(0,0,0,12,0,0),Bitis=new DateTime(12,30,0),DurusNedeni="Yemek Molası"},
+            new StandartDuruslar(){Id=3,Baslangıc=new DateTime(0,0,0,15,0,0),Bitis=new DateTime(15,15,0),DurusNedeni="Çay Molası"},
+        };
+    }
+    
+    
+   
+}
